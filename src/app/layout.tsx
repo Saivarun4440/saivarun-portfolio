0107978e2649a5021993e22ed75caa14a0044fe1
Saivarun Gandla <varungandla@Mac.attlocal.net>
2025-06-09 00:47:16 -0400

    Fix: removed hardcoded title to use layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,15 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
-  title: "Sai Venkat's Portfolio",
-  description: "A step towards learning about me.",
+  title: "Saivarun Gandla's Portfolio",
+  description: "About me.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans bg-black text-white min-h-screen`}>
